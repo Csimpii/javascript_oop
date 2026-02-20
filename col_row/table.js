059a5a7e04ea2eaf09a5e9ecea78dfe3cff75cff
Csimpii <revak.bence_2022b@bolyai-szakkozep.hu>
2026-02-20 09:52:27 +0100

    architect

--- col_row/table.js
+++ col_row/table.js
@@ -1,53 +1,173 @@
 /**
- * @callback tableCallback
+ * @import { FormFieldType, HeaderArrayType, ColspanType, RowspanType } from "./functions.js"
+ * 
+ * @callback TableCallback
  * @param {HTMLTableSectionElement} tbody
- * @param {ColspanType | RowspanType}
+ * @param {ColspanType | RowspanType} elem
  * @returns {void}
  */
-/**
- * @import {FormFieldType,HeaderArrayType,ColspanType,RowspanType} from './functions.js'
- */
+
+import { Manager } from "./manager.js";
 
 class Table{
     /**
      * @type {HTMLTableSectionElement}
      */
-    #tbody
-
+    #tbody;
     /**
      * @type {Manager}
      */
-    #manager
+    #manager;
+    // /**
+    //  * @type {boolean} - valtozo hogy megnezzuk hogy a table-unk rowpsanos-e
+    //  */
+    // #isRowspan;
 
-    constructor(HeaderArray, manager){
-        const table = document.createElement('table')
-        document.body.appendChild(table)
+    /**
+     * @param {HeaderArrayType} headerArray 
+     * @param {Manager} manager 
+     */
+    constructor(headerArray, manager) {
+        this.#manager = manager;
 
-        const thead = document.createElement('thead')
-        table.appendChild(thead)
+        // if ('szerzo2' in dataArray[0] && 'mu2' in dataArray[0] ) {
+        //     this.#isRowspan = true;
+        // } else {
+        //     this.#isRowspan = false;
+        // }
 
-        const tr = document.createElement('tr')
-        thead.body.appendChild(tr)
+        const table = document.createElement('table');
+        document.body.appendChild(table);
 
-        for(let e of HeaderArray){
-            const th = document.createElement("th")
+        const thead = document.createElement('thead');
+        table.appendChild(thead);
 
-            th.colSpan = e.ColspanType
-            this.innerText = 
-            tr.appendChild(th)
+        const tbody = document.createElement('tbody');
+        table.appendChild(tbody);
+        this.#tbody = tbody;
+
+        const tr = document.createElement('tr');
+        thead.appendChild(tr);
+
+        for (const cell of headerArray) {
+            const th = document.createElement('th');
+            tr.appendChild(th);
+            th.innerText = cell.name;
+
+            if (cell.colspan == 2) {
+                th.colSpan = 2;
+            }
         }
 
-        const tbody = document.createElement("tbody")
-        table.appendChild(tbody)
-        this.#tbody = tbody
 
+        // for (const row of dataArray) {
+        //     this.#addRow(row);
+        // }
     }
 
-    setAppendRow(){
-        this.#manager.addCallback = (element) =>{
+    // /**
+    //  * public manager calback fuggveny hogy meg tudjuk kulonboztetni a rowspanos es colspanos tsblazatot
+    //  */
+    // setManagerCallback() {
+    //     this.#manager.addCallback = (element) => {
+    //         this.#addRow(element);
+    //     }
+    // }
+
+
+    // /**
+    //  * hozzaadja a megfelelo formatumu sort 
+    //  * @param {ColspanType | RowspanType} data 
+    //  */
+    // #addRow(data) {
+    //     if (this.#isRowspan) {
+    //         this.#addRowspanRow(data);
+    //     }
+    //     else {
+    //         this.#addColspanRow(data);
+    //     }
+    // }
+
+    // /**
+    //  * Colspan típusú sor hozzaadasa
+    //  * @param {ColspanType} data 
+    //  */
+    // #addColspanRow(data) {
+    //     const tr = document.createElement('tr');
+    //     this.#tbody.appendChild(tr);
+
+    //     const tdNev = document.createElement('td');
+    //     tdNev.innerText = data.neve;
+    //     tr.appendChild(tdNev);
+
+    //     const tdKor = document.createElement('td');
+    //     tdKor.innerText = data.kor;
+    //     tr.appendChild(tdKor);
+
+    //     const tdSzerelme1 = document.createElement('td');
+    //     tdSzerelme1.innerText = data.szerelme1;
+    //     tr.appendChild(tdSzerelme1);
+
+    //     if (data.szerelme2) {
+    //         const tdSzerelme2 = document.createElement('td');
+    //         tdSzerelme2.innerText = data.szerelme2;
+    //         tr.appendChild(tdSzerelme2);
+    //     } else {
+    //         tdSzerelme1.colSpan = 2;
+    //     }
+    // }
+
+    
+
+    // /**
+    //  * Rowspan tipusu sor hozzaadasa
+    //  * @param {RowspanType} data 
+    //  */
+    // #addRowspanRow(data) {
+    //     const tr1 = document.createElement('tr');
+    //     this.#tbody.appendChild(tr1);
+
+    //     const tdNemzet = document.createElement('td');
+    //     tdNemzet.innerText = data.nemzet;
+    //     tr1.appendChild(tdNemzet);
+
+    //     const tdSzerzo = document.createElement('td');
+    //     tdSzerzo.innerText = data.szerzo;
+    //     tr1.appendChild(tdSzerzo);
+
+    //     const tdMu = document.createElement('td');
+    //     tdMu.innerText = data.mu;
+    //     tr1.appendChild(tdMu);
+
+    //     if (data.szerzo2 && data.mu2) {
+    //         tdNemzet.rowSpan = 2;
+
+    //         const tr2 = document.createElement('tr');
+    //         this.#tbody.appendChild(tr2);
+
+    //         const tdSzerzo2 = document.createElement('td');
+    //         tdSzerzo2.innerText = data.szerzo2;
+    //         tr2.appendChild(tdSzerzo2);
+
+    //         const tdMu2 = document.createElement('td');
+    //         tdMu2.innerText = data.mu2;
+    //         tr2.appendChild(tdMu2);
+    //     }
+    // }
+
+
+    /**
+     * 
+     * @param {TableCallback} tableCallback 
+     */
+    setAppendRow(tableCallback) {
+        this.#manager.addCallback = (element) => {
             tableCallback(this.#tbody, element)
         }
     }
 }
 
-export {Table}
+export { Table }
+
+// public fuggveny a tablenek ami besetteli a callbackjet a managernek
+// fuggetlenne teszi a table osztaly a colspan tablazattol
